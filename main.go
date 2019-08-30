b08019b2c7132cd692efb89f7d12cf801604b087
Felipe Galante <fgalante@levven.com>
2019-08-30 10:27:46 -0600

    [refactor]: Refactor main file to clean the code

--- main.go
+++ main.go
@@ -2,34 +2,50 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main() {
-
 	for {
-		isRunning := true
-		fmt.Println("\n1 - Start Monitoring")
-		fmt.Println("2 - Show logs")
-		fmt.Print("9 - Exit application\n\n")
+		printMenuOptions()
 
-		var option int
-		fmt.Scan(&option)
+		option := readOption()
 
-		switch option {
-		case 1:
-			fmt.Println("Monitoring...")
-		case 2:
-			fmt.Println("Monitoring...")
-		case 9:
-			fmt.Println("Exiting...")
-			isRunning = false
-			break
-		default:
-			fmt.Println("Unknown option!")
-		}
+		isRunning := handleOption(option)
+
 		if !isRunning {
-			break
+			// break
+			os.Exit(0)
 		}
 	}
+}
 
+func handleOption(option int) bool {
+	isRunning := true
+	switch option {
+	case 1:
+		fmt.Println("Monitoring...")
+	case 2:
+		fmt.Println("Logs:")
+	case 9:
+		fmt.Println("Exiting...")
+		isRunning = false
+		break
+	default:
+		fmt.Println("Unknown option!")
+	}
+	return isRunning
+}
+
+func printMenuOptions() {
+	fmt.Println("\n1 - Start Monitoring")
+	fmt.Println("2 - Show logs")
+	fmt.Print("9 - Exit application\n\n")
+}
+
+func readOption() int {
+	fmt.Println("Choose an option to start:")
+	var option int
+	fmt.Scan(&option)
+	return option
 }
